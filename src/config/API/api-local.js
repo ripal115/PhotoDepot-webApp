846b7e1359870438756984c46a03f1890dc6f285
ripal <ripal115.rejoice@gmail.com>
2022-06-14 10:41:17 +0530

    Profile create done

---
+++ src/config/API/api-local.js
@@ -0,0 +1,20 @@
+const protocol = "https";
+const host = "api.photodepot.rejoicehub.com/api/v1";
+
+// const protocol = "http";
+// const host = "192.168.29.171:3003/api/v1";
+
+const port = "";
+const trailUrl = "";
+
+const hostUrl = `${protocol}://${host}${port ? ":" + port : ""}`;
+const endpoint = `${protocol}://${host}${port ? ":" + port : ""}${trailUrl}`;
+
+export default {
+  protocol: protocol,
+  host: host,
+  port: port,
+  apiUrl: trailUrl,
+  endpoint: endpoint,
+  hostUrl: hostUrl,
+};
